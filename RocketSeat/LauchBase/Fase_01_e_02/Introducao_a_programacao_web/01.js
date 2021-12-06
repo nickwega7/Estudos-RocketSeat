35c83076e9b2514392fd5cf558f85683894aa20e
Nicolas W. S. Braga <nwsb27@gmail.com>
2021-12-05 22:52:06 -0300

    Primeira Postagem
    Aqui estou fazendo minha primeira criacao de diretorio, e primeiro arquivo adcionado ao Git

---
+++ RocketSeat/LauchBase/Fase_01_e_02/Introducao_a_programacao_web/01.js
@@ -0,0 +1,12 @@
+// Fazendo calculos com java script
+
+const aluno01 = 'Mayk'
+const aluno02 = "Diego"
+const aluno03 = `fulano`
+const notaAluno01 = 9.8
+const notaAluno02 = 10
+const notaAluno03 = 2
+
+const media = ((notaAluno01 + notaAluno02 + notaAluno03) / 3).toFixed(2)
+
+console.log(media)
